63a3f05788ed86b4eabcbe7345d8c2407dd2533a
2ps <p.shingavi@yahoo.com>
2017-12-13 20:41:22 -0800

    Updated wsl script with latest info
    Problem?
    
    We didn't have man pages under wsl because it is derived from a
    docker image that has the nodocs flag set.
    
    Solution!
    
    use sed to comment out the `nodocs` flag so that we install docs!

--- gists/wsl_centos_setup.sh
+++ gists/wsl_centos_setup.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# add the aws dns resolver to the environment
 grep '172.17.0.2' /etc/resolv.conf > /tmp/test
 if [ -f /tmp/test ] && [ ! -a /tmp/test ] ; then
     echo "nameserver 172.17.0.2" > /etc/resolv.conf.new
@@ -7,13 +8,15 @@ if [ -f /tmp/test ] && [ ! -a /tmp/test ] ; then
     cat /etc/resolv.conf >> /etc/resolv.conf.new
     mv -f /etc/resolv.conf.new /etc/resolv.conf
 fi
+# allow yum to install documentation like man
+sed -i -e 's,^tsflags=nodocs$,\# tsflags=nodocs,g' /etc/yum.conf
 rm -rf /tmp/test
 rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
 yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 yum -y install http://www.city-fan.org/ftp/contrib/yum-repo/city-fan.org-release-1-13.rhel7.noarch.rpm
 echo "[mariadb]" > /etc/yum.repos.d/mariadb.repo
 echo "name = MariaDB" >> /etc/yum.repos.d/mariadb.repo
-echo "baseurl = http://yum.mariadb.org/10.1/centos7-amd64" >> /etc/yum.repos.d/mariadb.repo
+echo "baseurl = http://yum.mariadb.org/10.2/centos7-amd64" >> /etc/yum.repos.d/mariadb.repo
 echo "gpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB" >> /etc/yum.repos.d/mariadb.repo
 echo "gpgcheck=1" >> /etc/yum.repos.d/mariadb.repo
 yum -y install https://centos7.iuscommunity.org/ius-release.rpm
@@ -52,44 +55,14 @@ yum -y install gnupg
 yum -y install libxml2-devel
 yum -y install libxslt-devel
 yum -y install tkinter tk-devel
+yum -y install python36u python27u git2u
 yum -y update
 yum -y upgrade
 yum -y install MariaDB-devel MariaDB-client MariaDB-shared
 yum -y install gettext-devel perl-CPAN perl-devel
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-if [ ! -e /usr/local/bin/git ] ; then
-    wget https://github.com/git/git/archive/v2.14.2.tar.gz -O /tmp/git.tgz
-    mkdir -p /usr/local/src/git
-    tar xzf /tmp/git.tgz -C /usr/local/src/git --strip-components=1
-    cd /usr/local/src/git && make configure && ./configure --prefix=/usr/local && make && make install
-    rm -rf /usr/local/src/git
-    rm -rf /tmp/git.tgz
-fi
 if [ ! -L /u ] && [ -d /mnt/c/u ] ; then
     ln -s /mnt/c/u /u ;
-fi
-mkdir -p /u/downloads
-if [ ! -e /usr/local/bin/python2.7 ] ; then
-    version="2.7.14"
-    wget "https://www.python.org/ftp/python/${version}/Python-${version}.tgz" -O "/u/downloads/python-${version}.tgz"
-    mkdir -p "/u/python-${version}"
-    cd "/u/python-${version}"
-    tar xzf "/u/downloads/python-${version}.tgz" --strip-components=1
-    ./configure && make && make altinstall
-    cd /u
-    rm -rf "/u/python-${version}"
-    rm -f "/u/downloads/python-${version}.tgz"
-fi
-if [ ! -e /usr/local/bin/python3.6 ] ; then
-    version="3.6.3"
-    wget "https://www.python.org/ftp/python/${version}/Python-${version}.tgz" -O "/u/downloads/python-${version}.tgz"
-    mkdir -p "/u/python-${version}"
-    cd "/u/python-${version}"
-    tar xzf "/u/downloads/python-${version}.tgz" --strip-components=1
-    ./configure && make && make altinstall
-    cd /u
-    rm -rf "/u/python-${version}"
-    rm -f "/u/downloads/python-${version}.tgz"
 fi
 if [ ! -e /usr/bin/git-crypt ] ; then
     mkdir -p /usr/local/src
@@ -140,7 +113,7 @@ if [ -e /u/to_penguins ] ; then
     if [ -d bin ] ; then rm -rf bin ; fi
     if [ -d lib ] ; then rm -rf lib ; fi
     if [ -d include ] ; then rm -rf include ; fi
-    virtualenv . -p /usr/local/bin/python2.7
+    virtualenv . -p /usr/bin/python2.7
     source bin/activate
     pip install six
     pip install -r requirements.txt
@@ -151,7 +124,7 @@ if [ -e /u/for_penguins ] ; then
     if [ -d bin ] ; then rm -rf bin ; fi
     if [ -d lib ] ; then rm -rf lib ; fi
     if [ -d include ] ; then rm -rf include ; fi
-    virtualenv . -p /usr/local/bin/python3.6
+    virtualenv . -p /usr/bin/python3.6
     source bin/activate
     pip install six
     pip install -r py3.requirements.txt
@@ -162,7 +135,7 @@ if [ -e /u/upholstery ] ; then
     if [ -d bin ] ; then rm -rf bin ; fi
     if [ -d lib ] ; then rm -rf lib ; fi
     if [ -d include ] ; then rm -rf include ; fi
-    virtualenv . -p /usr/local/bin/python2.7
+    virtualenv . -p /usr/bin/python2.7
     source bin/activate
     pip install -r requirements.txt
     deactivate
