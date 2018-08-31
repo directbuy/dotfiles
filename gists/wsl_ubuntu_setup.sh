2a965e2bc9731e5b44876dcf8757fc4829bc2673
2ps <pshingavi@gmail.com>
2018-08-31 02:33:08 -0500

    [OPS]  Added away colors for webstorm

--- gists/wsl_ubuntu_setup.sh
+++ gists/wsl_ubuntu_setup.sh
@@ -30,7 +30,7 @@ sudo apt install -y libmysqlclient-dev wget git redis curl libcurl4-openssl-dev 
     make automake autoconf nfs-common bind9utils rsync gnupg \
     libldap2-dev libsasl2-dev build-essential checkinstall \
     libreadline7 libreadline-dev libncursesw5-dev tk-dev libgdbm-dev libc6-dev \
-    git-crypt traceroute dnsutils net-tools
+    git-crypt traceroute dnsutils net-tools mysql-client-5.7
 if [[ ! -e /u ]] ; then sudo ln -s /mnt/c/u / ; fi
 mkdir -p /u/downloads
 build_python "2.7.15"
@@ -47,6 +47,13 @@ sudo apt update
 sudo ACCEPT_EULA=Y apt -y install msodbcsql17 mssql-tools
 echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bash_profile
 echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
+echo '[client]' >>/etc/mysql/my.cnf
+echo 'host=127.0.0.1' >>/etc/mysql/my.cnf
+if [[ ! -e /usr/local/bin/pip ]]; then
+    if [[ -e /usr/local/bin/pip3.6 ]] ; then
+        sudo ln -s /usr/local/bin/pip3.6 /usr/local/bin/pip ;
+    fi
+fi
 source ~/.bashrc
 sudo apt install -y unixodbc-dev pv
 sudo chsh -s /bin/zsh
