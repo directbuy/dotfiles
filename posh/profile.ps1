2dff3acbb6b062c757dd06d697abbd87168c71d2
2ps <pshingavi@gmail.com>
2020-09-19 22:42:25 -0500

    [OOPS]  Added windows profile for people to use.

--- posh/profile.ps1
+++ posh/profile.ps1
@@ -1,4 +1,6 @@
-﻿function connect($pshost) {
+﻿
+
+function connect($pshost) {
     $credential = Get-Credential
     $options = New-PSSessionOption -SkipCACheck -SkipCNCheck
     $session = New-PSSession -Credential $cred $pshost -UseSSL -SessionOption $options -Authentication Default
@@ -6,6 +8,225 @@
     Enter-PSSession $session
 }
 
+function setup_dircolors() {
+    $dircolors = "C:\u\dotfiles\zsh\.dircolors"
+    if (Test-Path $dircolors -and (!Test-Path "${env:HOMEDRIVE}${env:HOMEPATH}\.dircolors")) {
+        copy $dircolors "${env:HOMEDRIVE}${env:HOMEPATH}"
+        Update-DirColors $dircolors
+    }
+}
+
+function venv($name="") {
+    if (!(Test-Path ".wenv")) {
+        $installed_python = $null
+        $pythons = @{
+            "38" = "c:\python38\python.exe";
+            "37" = "c:\python37\python.exe";
+            "36" = "c:\python36\python.exe" }
+        foreach ($x in $pythons.keys) {
+            $python = $pythons[$x]
+            if (Test-Path $python) {
+                $python -m venv .wenv
+                break
+            }
+        }
+        Write-Host "venv created with $python"
+    }
+    else {
+        Write-Host "activating existing virtual environment"
+        djact
+    }
+    Write-Host "upgrading pip"
+    &python.exe -m pip -q install -U pip wheel setuptools | Out-Host
+    if (Test-Path ".\requirements.txt") {
+        Write-Host "installing requirements"
+        &pip -q install -r requirements.txt | Out-Host
+    }
+
+    if (Test-Path ".\setup.py") {
+        Write-Host "installing setup.py dependencies"
+        &pip -q install -e .
+    }
+}
+
+function venv($name="") {
+    $env_dir = "$env:homedrive$env:HOMEPATH\envs"
+    if (!(Test-Path $env_dir)) {
+        New-Item $env_dir -ItemType Directory
+    }
+    if ($name -eq "") {
+        $name = ([io.fileinfo]"$pwd").basename
+    }
+    $env_dirs = "${env_dir}\${name}", ".wenv"
+
+    if (!(Test-Path "$env_dir\$name")) {
+        $installed_python = $null
+        $pythons = @{
+            "38" = "c:\python38\python.exe";
+            "37" = "c:\python37\python.exe";
+            "36" = "c:\python36\python.exe" }
+        foreach ($x in $pythons.keys) {
+            $python = $pythons[$x]
+            if (Test-Path $python) {
+                mkvirtualenv $name -Python $python
+                $installed_python = $x
+                break
+            }
+        }
+        Write-Host "venv created with $python"
+        Write-Host "installing windows wheels"
+        &pip -q install C:\u\wheels\python_ldap-3.1.0-cp${installed_python}-cp${installed_python}m-win_amd64.whl | Out-Host
+    }
+    else {
+        Write-Host "activating existing virtual environment"
+        workon $name
+    }
+    Write-Host "upgrading pip"
+    &python.exe -m pip -q install -U pip wheel setuptools | Out-Host
+    if (Test-Path ".\requirements.txt") {
+        Write-Host "installing requirements"
+        &pip -q install -r requirements.txt | Out-Host
+    }
+
+    if (Test-Path ".\setup.py") {
+        Write-Host "installing setup.py dependencies"
+        &pip -q install -e .
+    }
+}
+
+
+function djact() {
+    if ((Test-Path ".wenv") -and (Test-Path ".wenv\scripts\activate.ps1")) {
+        . .wenv\scripts\activate.ps1
+    }
+    else {
+        $name = ([io.fileinfo]"$pwd").basename
+        workon $name
+    }
+}
+
+function dj() {
+    if (!(Test-Path .\manage.py)) {
+        Write-Host "no django found here"
+        return
+    }
+    &python manage.py @args
+}
+
+function djsp() {
+    dj shell_plus --quiet-load
+}
+
+function weather($city="Chicago") {
+    $weather = (curl "https://wttr.in/${city}").ParsedHtml.body.outerText
+    $weather = $weather -split "`r`n"
+    for ($x = 0; $x -lt 17; ++$x) {
+        Write-Host $weather[$x]
+    }
+}
+
+$env:virtual_env_disable_prompt=1
+function global:prompt {
+    $dir = "$pwd".toLower().replace("\", "/");
+    $virtualenv = $env:VIRTUAL_ENV
+    $commit = $null
+    $output = git status --porcelain --branch 2>$null | Out-String
+    $branch = $null
+    $dirty = $false
+    $commit = $null
+    if ($output) {
+        $output = $output -split "`r`n"
+        $branch = $output[0]
+        $branch = $branch.split('.')[0].substring(3)
+        if ($branch -eq "No commits yet on master") {
+            $branch = "<empty>"
+        }
+        if ($output.Count -gt 2) {
+            $dirty = $true
+        }
+        $commit = git rev-parse --short HEAD 2>&1
+    }
+    $pieces1 = New-Object System.Collections.ArrayList;
+    $length1 = 3
+    $z = $pieces1.Add(($dir, [ConsoleColor]::Gray))
+    $length1 += 2 + $dir.Length
+    if ($virtualenv -and ($virtualenv.Length -gt 0)) {
+        $virtualenv = Split-Path $virtualenv -Leaf
+        $z = $pieces1.Add(("($virtualenv)", [ConsoleColor]::DarkCyan))
+        $length1 += 6 + $virtualenv.Length
+    }
+    if ($commit) {
+        $commit = $commit.substring(0, 7);
+        $piece = "[$branch $commit]"
+        $z = $pieces1.Add(($piece, [ConsoleColor]::DarkCyan))
+        $length1 += 4 + $piece.Length
+        # $line1 = "$line1━━ [$branch $commit] ";
+
+        if ($dirty) {
+            $z = $pieces1.Add(("*", [ConsoleColor]::White))
+            $length1 += 5
+            # $line1 = "$line1* "
+        }
+    }
+    $dt = Get-Date -UFormat "%Y.%m.%d %I:%M %p"
+    $computer = $env:COMPUTERNAME
+    $user = $env:USERNAME
+    $pieces2 = New-Object System.Collections.ArrayList
+    $length2 = 3
+    $z = $pieces2.Add(("posh", [ConsoleColor]::DarkGreen))
+    $length2 += 6
+    $piece = "${user}@${computer}"
+    $z = $pieces2.Add(($piece, [ConsoleColor]::Gray))
+    $length2 += 4 + $piece.Length
+    $z = $pieces2.Add(($dt, [ConsoleColor]::Gray))
+    $length2 += 4 + $dt.Length
+    $padding1 = ""
+    $padding2 = ""
+    if ($length1 -lt $length2) {
+        $padding1 = ""
+        for ($x = 0; $x -lt ($length2 - $length1); $x++) {
+            $padding1 += "━"
+        }
+    }
+    elseif ($length2 -lt $length1) {
+        for ($x = 0; $x -lt ($length1 - $length2); $x++) {
+            $padding2 += "━"
+        }
+    }
+    Write-Host
+    Write-Host "┏" -ForegroundColor DarkGreen -NoNewline
+    foreach ($x in $pieces1) {
+        $st = $x[0];
+        $color = $x[1];
+        Write-Host "━━" -ForegroundColor DarkGreen -NoNewline
+        Write-Host " $st " -ForegroundColor $color -NoNewline
+    }
+    if ($padding1) {
+        Write-Host $padding1 -ForegroundColor DarkGreen -NoNewline
+    }
+    Write-Host "━┓" -ForegroundColor DarkGreen
+    Write-Host "┣" -ForegroundColor DarkGreen -NoNewline
+    foreach ($x in $pieces2) {
+        $st = $x[0];
+        $color = $x[1];
+        Write-Host "━━" -ForegroundColor DarkGreen -NoNewline
+        Write-Host " $st " -ForegroundColor $color -NoNewline
+    }
+    if ($padding2) {
+        Write-Host $padding2 -ForegroundColor DarkGreen -NoNewline
+    }
+    Write-Host "━┛" -ForegroundColor DarkGreen
+    Write-Host "┗ " -nonewline -ForegroundColor DarkGreen
+    Write-Host "➤" -foregroundcolor white -nonewline
+    return " "
+}
+
+
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
+Import-Module DirColors
+ipmo DockerCompletion
+setup_dircolors
+Set-PSReadlineKeyHandler -Key Ctrl+d -Function DeleteCharOrExit
 
 # SIG # Begin signature block
 # MIIM8gYJKoZIhvcNAQcCoIIM4zCCDN8CAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
