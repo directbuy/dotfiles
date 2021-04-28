5d6be765530464ed6a9fe3bcfc207bbfe6b98292
wthompson <wthompson@directbuy.com>
2021-04-28 15:55:28 -0500

    removed redundant or useless functions'

--- posh/Modules/fleeting_fling.psm1
+++ posh/Modules/fleeting_fling.psm1
@@ -33,36 +33,3 @@ function start_fling() {
 Set-Alias fling go_fling
 Set-Alias psdir gotoPowerShellDirectory
 Set-Alias cu u_dir
-
-<#  function for fun and example of how to isue webrequest in POSH #>
-function weather($city="Chicago") {
-    $weather = (Invoke-WebRequest "https://wttr.in/${city}" -UserAgent "curl").content
-    $weather = $weather -split "`n"
-    for ($x = 0; $x -lt 17; ++$x) {
-        Write-Host $weather[$x]
-    }
-}
-
-<# function as example of some basic POSH script syntax #>
-function goodMorning {
-    [OutputType([string])]
-    Param
-    (
-        # Say hello to
-        [Parameter(Mandatory=$false, ValueFromPipelineByPropertyName=$true, Position=0)]
-        [string]$To
-    )
-    Begin
-    {
-        if(-not $To)
-        {  
-            Write-Verbose 'Retrieving current username'         
-            $To = $env:Username
-        }
-    }
-    Process
-    {
-        Write-Host "Hello $To" -ForegroundColor Green
-    }
-    End {}    
-}
